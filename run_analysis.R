afeb45e96ead7262fbba446a944727f114e67755
mjayespina <54915517+mjayespina@users.noreply.github.com>
2019-11-27 22:25:29 -0600

    Add files via upload

---
+++ run_analysis.R
@@ -0,0 +1,9 @@
+# BioC5361 Project
+# Title: Run Analysis
+# Group : Group 2
+# filename: run_analysis.R
+# Author: Mary Jane Espina, Claire Peichel , Sarah Lichtenberger
+
+
+source("~/Desktop/animal_plant_diet.R")
+
